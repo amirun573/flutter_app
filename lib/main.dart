d220ff5f3656fbc747e4ea721d693b77cc9b5f85
amirun573 <amirunaqmar@gmail.com>
2020-12-28 16:31:43 +0800

    using margin and padding

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,20 @@ class MyApp extends StatelessWidget {
           child: Container(
             height: 100,
             width: 100,
-            margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
+            margin: EdgeInsets.only(left: 30.0),
+            padding: EdgeInsets.all(20.0),
             color: Colors.white,
             child: Text('hello'),
           ),
         ),
       ),
     );
+  }
+}
+
+class MyNew extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }

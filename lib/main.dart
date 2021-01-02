bc8c1b60040ec1215002975cb11a9626450e1ffd
amirun573 <amirunaqmar@gmail.com>
2021-01-02 11:46:50 +0800

    Add Email

--- lib/main.dart
+++ lib/main.dart
@@ -26,8 +26,51 @@ class MyApp extends StatelessWidget {
               ),
             ),
             Container(
+              padding: EdgeInsets.all(10.0),
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
               child: Row(
-                children: <Widget>[Icon(Icons.add_shopping_cart, size: 100.0)],
+                children: <Widget>[
+                  Icon(
+                    Icons.phone,
+                    size: 100.0,
+                    color: Colors.teal.shade900,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    '+0136365573',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Langar',
+                      fontSize: 20.0,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.red,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    'amirunaqmar@gmail.com',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Langar',
+                    ),
+                  )
+                ],
               ),
             ),
           ],

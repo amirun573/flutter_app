d7205117623c345b1be27973f0254e8f00217b09
amirun573 <amirunaqmar@gmail.com>
2021-01-02 12:09:07 +0800

    Used Card Instaed Of Container

--- lib/main.dart
+++ lib/main.dart
@@ -25,33 +25,34 @@ class MyApp extends StatelessWidget {
                 color: Colors.white,
               ),
             ),
-            Container(
-              padding: EdgeInsets.all(10.0),
+            Card(
               color: Colors.white,
               margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.phone,
-                    size: 100.0,
-                    color: Colors.teal.shade900,
-                  ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
-                    '+0136365573',
-                    style: TextStyle(
+              child: Padding(
+                padding: EdgeInsets.all(25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      size: 100.0,
                       color: Colors.teal.shade900,
-                      fontFamily: 'Langar',
-                      fontSize: 20.0,
                     ),
-                  )
-                ],
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+0136365573',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Langar',
+                        fontSize: 20.0,
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
-            Container(
-              padding: EdgeInsets.all(10.0),
+            Card(
               margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
               child: Row(
                 children: <Widget>[
